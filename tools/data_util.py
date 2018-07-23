35e8d173029f8f28ab47e7adc668cad47e5ea8ae
lnest <tongwendi@bbdservice.com>
2018-07-23 19:59:05 +0800

    day 0723

--- tools/data_util.py
+++ tools/data_util.py
@@ -5,18 +5,18 @@
 # Edit Author: lnest
 # ------------------------------------
 import os
+import json
 import pandas as pd
 
+from time_eval import time_count
+
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(CURRENT_DIR, '../data/ccnews/')
-DEFAULT_PATH = [DATA_PATH + './cnews.text.txt', DATA_PATH + './cnews.train.txt', DATA_PATH + './cnews.val.txt']
+DATA_PATH = os.path.join(CURRENT_DIR, '../data/new_data/')
+DEFAULT_PATH = [DATA_PATH + './test_set.csv', DATA_PATH + './train_set.csv']
 
 
-def explore_data(tf, df, datas=DEFAULT_PATH, line_seperator=',', cont_sep=' ', target_index=0, has_header=True):
+def explore_data(datas=DEFAULT_PATH, line_seperator=',', cont_sep=' ', target_index=0, has_header=True):
     """
-
-    :param tf:
-    :param df:
     :param datas:
     :param line_seperator:
     :param cont_sep:
@@ -24,22 +24,27 @@ def explore_data(tf, df, datas=DEFAULT_PATH, line_seperator=',', cont_sep=' ', t
     :param has_header:
     :return:
     """
+    df = dict()
+    tf = dict()
     corpus_size = 0
     assert isinstance(datas, list), 'datas should be a list'
     for data in datas:
         for line in open(data):
             if corpus_size == 0 and has_header:
+                has_header = False
                 continue
             conts = line.strip().split(line_seperator)
             if target_index < len(conts):
                 target_words = conts[target_index].strip().split(cont_sep)
                 uniq_words = set(target_words)
+                print('1', len(target_words))
+                print('2', len(uniq_words))
                 for word in target_words:
                     tf.setdefault(word, 0)
                     tf[word] += 1
                 for word in uniq_words:
                     df.setdefault(word, 0)
-                    tf[word] += 1
+                    df[word] += 1
                 corpus_size += 1
     return tf, df, corpus_size
 
@@ -48,5 +53,38 @@ def show_data(tf, df, corpus_size):
     dataframe = pd.DataFrame({'tf': tf, 'df': df})
     dataframe = dataframe / corpus_size
     sorted_df = dataframe.sort_values(by=['df'], ascending=False)
-    sorted_df.head()
+    head_df = sorted_df.head(20)
+    return head_df
 
+
+def save_df(save_path, df):
+    df.to_csv(save_path)
+
+
+@time_count
+def generate_wordmap(word_map_file, data_dir=DEFAULT_PATH):
+    if not os.path.exists(data_dir):
+        print('invalid annotated data directory: {}'.format(data_dir))
+        return
+
+    word2idx = {'PAD': 0, 'SOS': 1, 'EOS': 2, 'UNK': 3}
+    idx2word = {0: 'PAD', 1: 'SOS', 2: 'EOS', 3: 'UNK'}
+    word_cnt = len(word2idx)
+
+    for corpus_file in data_dir:
+        with open(corpus_file, 'rb') as fr:
+            lines = fr.readlines()
+            for line in lines:
+                line = line.decode('utf-8').strip()
+                words = list(line)
+                for word in words:
+                    if word not in word2idx:
+                        word2idx[word] = word_cnt
+                        idx2word[word_cnt] = word
+                        word_cnt += 1
+    word_map = {'word2idx': word2idx, 'idx2word': idx2word}
+    json.dump(word_map, open(word_map_file, 'w'), ensure_ascii=False)
+
+
+def process():
+    pass
---
+++ tools/time_eval.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------------
+# Create On 2018/5/24 14:00 
+# File Name: time_eval.py
+# Edit Author: lnest
+# ------------------------------------
+
+import time
+from functools import wraps
+
+
+def time_count(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        s_time = time.time()
+        data = func(*args, **kwargs)
+        e_time = time.time()
+        print('run {} cost: {}'.format(func.__name__, e_time - s_time))
+        return data
+    return wrapper
+
+
+class TimeCountBlock:
+    def __init__(self, name=None):
+        self._name = name
+        self._timepast = 0
+
+    def __enter__(self):
+        self._start = time.time()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._end = time.time()
+        self._timepast = self._end - self._start
+        print(u'{} : {}'.format(self._name, round(self._timepast, 3)))

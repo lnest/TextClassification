555d3f5b2f06e8dff98f63218ced5a906d2c2629
lnest <tongwendi@bbdservice.com>
2018-08-12 15:18:38 +0800

    0812

--- tools/data_util.py
+++ tools/data_util.py
@@ -20,12 +20,16 @@ LOGGER = logging.getLogger()
 
 
 @time_count
-def read_corpus(corpus_path, line_seperator=',', cont_sep=' ', article_index=0, segged_index=1, label_index=None, has_header=True):
+def read_corpus(corpus_path, line_seperator=',', cont_sep=' ', stop_words=None, segged_stop_words=None, article_index=0, segged_index=1, label_index=None, has_header=True):
     corpus_size = 0
     corpus = list()
     if not os.path.exists(corpus_path):
         LOGGER.warning('path {} do not exsit!'.format(corpus_path))
         sys.exit(1)
+
+    if stop_words and segged_stop_words:
+        LOGGER.info('length stop words: {}'.format(len(stop_words)))
+        LOGGER.info('length segged stop words: {}'.format(len(segged_stop_words)))
 
     for line in tqdm(open(corpus_path).readlines()):
 
@@ -35,11 +39,17 @@ def read_corpus(corpus_path, line_seperator=',', cont_sep=' ', article_index=0, 
             has_header = False
             continue
         conts = line.strip().split(line_seperator)
+
+        # get article and segged_words, then filt corpus using stop words
         if article_index < len(conts):
             article = conts[article_index].strip().split(cont_sep)
+            if stop_words and isinstance(stop_words, set):
+                article = [char for char in article if char not in stop_words]
 
         if segged_index < len(conts):
             segged_word = conts[segged_index].strip().split(cont_sep)
+            if segged_stop_words and isinstance(segged_stop_words, set):
+                segged_word = [word for word in segged_word if word not in segged_stop_words]
 
         if article and segged_word:
             # train dataset has a lable, conversely test dataset is not
@@ -121,38 +131,37 @@ def write_data(data, file_path):
 def get_tf(article):
     tf = dict()
     for token in article:
+        token = int(token)
         tf.setdefault(token, 0)
         tf[token] += 1
     tf_series = pd.Series(tf)
-    tf_series = tf_series.div(len(article))
+    with TimeCountBlock('tf div article'):
+        tf_series = tf_series.div(len(article))
     return tf_series
 
 
-def get_corpus_feature(idf, article, token2idx_map, feature_len=100, stop_words=None):
+@time_count
+def get_corpus_feature(idf, article, token2idx_map, feature_len=100):
     """ get the first 100 words by tf-idf
     :return:
     """
-    filter_words = list()
+    # with TimeCountBlock('get tf'):
     tf = get_tf(article)
+    # with TimeCountBlock('get idf'):
+    LOGGER.debug('tf shape: {}'.format(tf.shape))
+    LOGGER.debug('idf shape: {}'.format(idf.shape))
     tf_idf = tf.mul(idf)
-    if stop_words is not None:
-        with TimeCountBlock('keep_index'):
-            tf_idf_index = set(tf_idf.index)
-            keep_index = tf_idf_index.difference(stop_words)
-        with TimeCountBlock('tf_idf'):
-            tf_idf = tf_idf[tf_idf.index.isin(keep_index)]
+    
     with TimeCountBlock('sort_values'):
-        feature_index = tf_idf.sort_values(ascending=False).head(feature_len).index
-    for word in article:
-        if word in feature_index:
-            filter_words.append(word)
+        feature_index = tf_idf.nlargest(feature_len).index
+    filter_words = [word for word in article if word in feature_index]
     with TimeCountBlock('corpus2idx'):
         filter_idx = corpus2idx(filter_words, token2idx_map, max_para_len=feature_len)
     return filter_idx
 
 
 @time_count
-def generate_dl_data(corpus, args, word_map, segged_map=None, idf=None, stop_words=None, segged_stop_words=None, max_para_len=None, dev_ratio=0.1):
+def generate_dl_data(corpus, args, word_map, segged_map=None, idf=None, max_para_len=None, dev_ratio=0.1):
     if word_map is None:
         LOGGER.error('word map is None!')
         return
@@ -171,21 +180,19 @@ def generate_dl_data(corpus, args, word_map, segged_map=None, idf=None, stop_wor
             sample['para'] = corpus2idx(sample['para'], word_map, max_para_len)
             sample['segged_words'] = corpus2idx(sample['segged_word'], segged_map, max_para_len)
         else:
-            with TimeCountBlock(name='get_corpus_feature_para'):
-                sample['para'] = get_corpus_feature(idf.get('word'), sample['para'], word_map, max_para_len, stop_words)
-            sample['segged_words'] = get_corpus_feature(idf.get('segged'), sample['segged_word'], segged_map, max_para_len, segged_stop_words)
-        if cnt >= 3:
-            break
+            sample['para'] = get_corpus_feature(idf.get('word'), sample['para'], word_map, max_para_len)
+            sample['segged_word'] = get_corpus_feature(idf.get('segged'), sample['segged_word'], segged_map, max_para_len)
         str_sample = json.dumps(sample)
         if pivot > 0 and cnt <= pivot:
             dev_buffer.append(str_sample)
         else:
             train_buffer.append(str_sample)
+
     train_data = '\n'.join(train_buffer)
     dev_data = '\n'.join(dev_buffer)
-    write_data(train_data, '_'.join([args.idx_path, args.data_format, args.mode]))
+    write_data(train_data, '_'.join([args.idx_path, args.mode]))
     if args.mode == 'train':
-        write_data(dev_data, '_'.join([args.idx_path, args.data_format, 'dev']))
+        write_data(dev_data, '_'.join([args.idx_path, 'dev']))
 
     return len(train_buffer), len(dev_buffer)
 
@@ -209,10 +216,6 @@ def process(args):
     # tokens map
     word_map = generate_wordmap('./data/maps/token_map_char', target_index=1)
     segged_map = generate_wordmap('./data/maps/token_map_word', target_index=2)
-    # corpus
-    is_train = True if args.mode == 'train' else False
-    corpus_file = DEFAULT_PATH[0] if is_train else DEFAULT_PATH[1]
-    corpus_size, corpus = read_corpus(corpus_file, article_index=1, segged_index=2)
 
     # stop words
     segged_stop_words_path = os.path.join(args.dinfo_path, 'stop_words_segged')
@@ -220,10 +223,26 @@ def process(args):
     stop_words_path = os.path.join(args.dinfo_path, 'stop_words')
     stop_words = load_stop_words(stop_words_path)
 
+    # corpus
+    is_train = True if args.mode == 'train' else False
+    corpus_file = DEFAULT_PATH[0] if is_train else DEFAULT_PATH[1]
+    corpus_size, corpus = read_corpus(corpus_file, stop_words=stop_words, segged_stop_words=segged_stop_words, article_index=1, segged_index=2)
+
     # idf
     segged_idf = get_idf(os.path.join(args.dinfo_path, 'seg_corpus_info'))
     word_idf = get_idf(os.path.join(args.dinfo_path, 'word_corpus_info'))
-    idf = {'word': word_idf, 'segged': segged_idf}
 
-    size = generate_dl_data(corpus, args, word_map, segged_map, stop_words=stop_words, segged_stop_words=segged_stop_words, idf=idf)
+    # use stop words filter
+    print('before filt:', segged_idf.shape)
+    keep_index_seg = set([str(row_label) for row_label in segged_idf.index]) - segged_stop_words
+    segged_idf = segged_idf[segged_idf.index.isin(keep_index_seg)]
+    print('After filt:', segged_idf.shape)
+
+    print('before filt:', word_idf.shape)
+    keep_index = set([str(row_label) for row_label in word_idf.index]) - stop_words
+    word_idf = word_idf[word_idf.index.isin(keep_index)]
+    idf = {'word': word_idf, 'segged': segged_idf}
+    print('After filt:', word_idf.shape)
+
+    size = generate_dl_data(corpus, args, word_map, segged_map, idf=idf, max_para_len=100)
     print(size)
